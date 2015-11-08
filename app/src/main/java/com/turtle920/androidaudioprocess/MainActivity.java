241bc83b0aa46e11a85acb04ce07e9e636453997
Turtle920 <370110405@qq.com>
2015-11-09 01:25:25 +0800

    good compass+mag=orientation

--- app/src/main/java/com/turtle920/androidaudioprocess/MainActivity.java
+++ app/src/main/java/com/turtle920/androidaudioprocess/MainActivity.java
@@ -7,48 +7,70 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
 
-import java.text.DecimalFormat;
-
-public class MainActivity extends Activity implements SensorEventListener {
+//实现传感器事件监听：SensorEventListener
+public class MainActivity extends Activity implements SensorEventListener{
 
     private SensorManager sensorManager;
-    private Sensor sensor;
-    private float[] gravity = {10, 10, 10};
-    private float[] acc = new float[3];
+    private Sensor acc_sensor;
+    private Sensor mag_sensor;
+    //加速度传感器数据
+    float accValues[]=new float[3];
+    //地磁传感器数据
+    float magValues[]=new float[3];
+    //旋转矩阵，用来保存磁场和加速度的数据
+    float r[]=new float[9];
+    //模拟方向传感器的数据（原始数据为弧度）
+    float values[]=new float[3];
+    int counter=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);//TYPE_GYROSCOPE
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
+        acc_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mag_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        //给传感器注册监听：
+        sensorManager.registerListener(this, acc_sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, mag_sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    @Override  /* 对于陀螺仪，测量的是x、y、z三个轴向的角速度，分别从values[0]、values[1]、values[2]中读取，单位为弧度/秒。*/
+    //传感器状态改变时的回调方法
+    @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-            showInfo("事件：" + " x:" + event.values[0] + " y:" + event.values[1] + " z:" + event.values[2]);
-
-        for (int i = 0; i < 3; i++) {
-            gravity[i] = (float) (0.1 * event.values[i] + 0.9 * gravity[i]);
-            acc[i] = event.values[i] - gravity[i];
+        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            accValues=event.values;
         }
-        DecimalFormat df = new DecimalFormat("0.00000 ");
-        Log.e("acc", "acc:" + df.format(acc[0]) + " " + df.format(acc[1]) + " " + df.format(acc[2]));
-    }
+        else if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD){
+            magValues=event.values;
+        }
+        /**public static boolean getRotationMatrix (float[] R, float[] I, float[] gravity, float[] geomagnetic)
+         * 填充旋转数组r
+         * r：要填充的旋转数组
+         * I:将磁场数据转换进实际的重力坐标中 一般默认情况下可以设置为null
+         * gravity:加速度传感器数据
+         * geomagnetic：地磁传感器数据
+         */
+        SensorManager.getRotationMatrix(r, null, accValues, magValues);
+        /**
+         * public static float[] getOrientation (float[] R, float[] values)
+         * R：旋转数组
+         * values ：模拟方向传感器的数据
+         */
 
-    //在华为P6的机器上，陀螺仪非常敏感，平放在桌面，由于电脑照成的轻微震动在不断地刷屏，为了避免写UI造成的性能问题，只写Log。
-    private void showInfo(String info) {
-        //tv.append("\n" + info);
-        //Log.d("陀螺仪", info);
+        SensorManager.getOrientation(r, values);
+
+        if (counter++ % 10==1){
+            Log.e("DEBUG", "x:"+Math.toDegrees(values[0])+" y:"+Math.toDegrees(values[1])+" z:"+Math.toDegrees(values[2]));
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
 }
