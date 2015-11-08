ca260c78dff9959437c06c1e42f2ee324fc43e58
Turtle920 <370110405@qq.com>
2015-11-08 19:52:14 +0800

    3 button

--- app/src/main/java/com/turtle920/androidaudioprocess/MainActivity.java
+++ app/src/main/java/com/turtle920/androidaudioprocess/MainActivity.java
@@ -5,24 +5,57 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp = new MediaPlayer();
+    String song = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Music/tf.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
 
-        MediaPlayer mp = new MediaPlayer();
-        String song = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Music/tf.mp3";
         try {
             mp.setDataSource(song);
             mp.prepare();
-            mp.start();
         } catch (Exception e) {
             Log.e("DEBUG", "" + e.toString());
             e.printStackTrace();
         }
+
+        Button button1 = (Button) findViewById(R.id.button_start);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button_pause);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.pause();
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button_replay);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.stop();
+                try {
+                    mp.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
