e8bbf4b99d50cc14aa8baee58cb591517923d0cc
Turtle920 <370110405@qq.com>
2015-11-08 22:09:38 +0800

    good gyroscope

--- app/src/main/java/com/turtle920/androidaudioprocess/MainActivity.java
+++ app/src/main/java/com/turtle920/androidaudioprocess/MainActivity.java
@@ -1,85 +1,45 @@
 package com.turtle920.androidaudioprocess;
 
-import android.media.MediaPlayer;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
-import java.io.IOException;
+public class MainActivity extends Activity implements SensorEventListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    MediaPlayer mp = new MediaPlayer();
-    float leftVol=0.5f, rightVol=0.5f;
-    String song = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Music/tf.mp3";
+    private SensorManager sensorManager;
+    private Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            mp.setDataSource(song);
-            mp.prepare();
-            mp.setVolume(leftVol,rightVol);
-        } catch (Exception e) {
-            Log.e("DEBUG", "" + e.toString());
-            e.printStackTrace();
-        }
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-        Button button1 = (Button) findViewById(R.id.button_start);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mp.start();
-            }
-        });
+    }
 
-        Button button2 = (Button) findViewById(R.id.button_pause);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mp.pause();
-            }
-        });
+    @Override  /* 对于陀螺仪，测量的是x、y、z三个轴向的角速度，分别从values[0]、values[1]、values[2]中读取，单位为弧度/秒。*/
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE)
+            showInfo("事件：" + " x:" + event.values[0] + " y:" + event.values[1]  + " z:" + event.values[2]);
+    }
 
-        Button button3 = (Button) findViewById(R.id.button_replay);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mp.stop();
-                try {
-                    mp.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+    //在华为P6的机器上，陀螺仪非常敏感，平放在桌面，由于电脑照成的轻微震动在不断地刷屏，为了避免写UI造成的性能问题，只写Log。
+    private void showInfo(String info){
+        //tv.append("\n" + info);
+        Log.d("陀螺仪",info);
+    }
 
-        Button button4 = (Button)findViewById(R.id.button_l);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (leftVol+0.05f<=1.0f) leftVol+=0.05f;
-                if (rightVol-0.05f>=0f) rightVol-=0.05f;
-                Log.e("DEBUG",""+leftVol+" "+rightVol);
-                mp.setVolume(leftVol, rightVol);
-            }
-        });
-
-        Button button5 = (Button)findViewById(R.id.button_r);
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (leftVol-0.05f>=0f) leftVol-=0.05f;
-                if (rightVol+0.05f<=1.0f) rightVol+=0.05f;
-                Log.e("DEBUG",""+leftVol+" "+rightVol);
-                mp.setVolume(leftVol, rightVol);
-            }
-        });
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
+
+// ...
